4e29af696f58c6a4da2c86dc387db13b9dc02b3d
Del Harvey <derekh@disruptech.co.uk>
2013-12-02 13:35:13 +0000

    WIP

--- tweet.js
+++ tweet.js
@@ -24,7 +24,7 @@
 	String.prototype.parseTwitterHashtag = function() {
 		return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
 			var tag = t.replace("#","%23")
-			return t.link("http://search.twitter.com/search?q="+tag);
+			return t.link("http://www.twitter.com/search?q="+tag);
 		});
 	};
 
